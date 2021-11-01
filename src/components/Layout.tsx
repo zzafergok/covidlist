bfbfe92553f10b26ab8e758e38d8a19249a2a0b3
Zafer Gok <zfrgk96@gmail.com>
2021-11-01 12:27:16 +0300

    add logo and site name

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import Header from "./Header";
+import React from 'react';
+import Header from './Header';
+import logo from '../assets/images/logo.png';
 
 interface IProps {
   children: React.ReactNode;
 }
 
-const Layout: React.StatelessComponent<IProps> = ({ children }) => {
+const Layout = ({ children }: IProps) => {
   return (
     <>
-      <Header />
+      <Header siteName={process.env.REACT_APP_WEB_SITE_TITLE} image={logo} />
       {children}
     </>
   );
